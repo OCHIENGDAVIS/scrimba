414f9cd7343fd495c52410f4dc8cdb55083634ab
Davis <davisochieng4@gmail.com>
2020-01-10 02:37:08 -0800

    Updates

---
+++ precache-manifest.db29a78d0fee7262049311bac9c5c5e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538b0c3013accd94284cb5b5cd1af11a",
+    "url": "/scrimba/index.html"
+  },
+  {
+    "revision": "68cdc3b039c8f0fc9ff2",
+    "url": "/scrimba/static/css/main.f2287745.chunk.css"
+  },
+  {
+    "revision": "7305c75d2d4c918d57a0",
+    "url": "/scrimba/static/js/2.860e1ad4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/scrimba/static/js/2.860e1ad4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68cdc3b039c8f0fc9ff2",
+    "url": "/scrimba/static/js/main.51376a17.chunk.js"
+  },
+  {
+    "revision": "f967733ddcc67b37b0cd",
+    "url": "/scrimba/static/js/runtime-main.b6011c7a.js"
+  }
+]);
